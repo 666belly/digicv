6f84523bf0eba1bf990189ce271fa7c0cd566d86
Isabel <124281590+666belly@users.noreply.github.com>
2024-09-18 15:11:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
   let index = 0;
 
   function typeLetter() {
-    // Build the string up to the current index
     typewriterElement.innerHTML = textToType.substring(0, index + 1);
     index++;
 
     if (index < textToType.length) {
-      setTimeout(typeLetter, 100); // Typing speed
+      setTimeout(typeLetter, 100); 
     }
   }
 
-  setTimeout(typeLetter, 500); // Start delay
-});
-
-document.querySelectorAll(".sidebar a").forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
+  setTimeout(typeLetter, 500); 
 });
